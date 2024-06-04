30c075cacaae8aa696928c54fdd410ad9bb7d9c5
Patrick Jakobsen <97399891+Pallepadehat@users.noreply.github.com>
2024-06-04 20:34:14 +0200

    Update NotchView.swift

--- Sources/DynamicNotchKit/NotchView.swift
+++ Sources/DynamicNotchKit/NotchView.swift
@@ -21,11 +21,11 @@ struct NotchView: View {
                         .blur(radius: self.dynamicNotch.isVisible ? 0 : 10)
                         .scaleEffect(self.dynamicNotch.isVisible ? 1 : 0.8)
                         .padding(.horizontal, 15)    // Small corner radius of the TOP of the notch
-                        .frame(minHeight: 20)
+                        .frame(width: self.dynamicNotch.customWidth ?? self.notchSize.width, height: self.dynamicNotch.customHeight ?? self.notchSize.height)
                         .padding(.top, self.isInfo ? -20 : 0)
                 }
                 .fixedSize()
-                .frame(minWidth: self.dynamicNotch.customWidth ?? self.notchSize.width)
+                .frame(width: self.dynamicNotch.customWidth ?? self.notchSize.width, height: self.dynamicNotch.customHeight ?? self.notchSize.height)
                 .onHover { hovering in
                     dynamicNotch.isMouseInside = hovering
                 }
@@ -40,8 +40,8 @@ struct NotchView: View {
                             Spacer(minLength: 0)
                             NotchShape(cornerRadius: self.dynamicNotch.isVisible ? 20 : nil)
                                 .frame(
-                                    width: self.dynamicNotch.isVisible ? self.dynamicNotch.customWidth : self.notchSize.width,
-                                    height: self.dynamicNotch.isVisible ? self.dynamicNotch.customHeight : self.notchSize.height
+                                    width: self.dynamicNotch.isVisible ? (self.dynamicNotch.customWidth ?? self.notchSize.width) : self.notchSize.width,
+                                    height: self.dynamicNotch.isVisible ? (self.dynamicNotch.customHeight ?? self.notchSize.height) : self.notchSize.height
                                 )
                             Spacer(minLength: 0)
                         }
