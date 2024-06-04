19c33ef84ecb2d66ea8375714268df028736a4d1
Patrick Jakobsen <97399891+Pallepadehat@users.noreply.github.com>
2024-06-04 21:04:13 +0200

    Fix

--- Sources/DynamicNotchKit/DynamicNotch.swift
+++ Sources/DynamicNotchKit/DynamicNotch.swift
@@ -72,8 +72,13 @@ public class DynamicNotch: ObservableObject {
 
         self.initializeWindow(screen: screen)
 
+        let targetWidth = customWidth ?? DynamicNotch.getNotchSize(screen: screen).width
+        let targetHeight = customHeight ?? DynamicNotch.getNotchSize(screen: screen).height
+
         DispatchQueue.main.async {
             withAnimation(self.animation) {
+                self.notchWidth = targetWidth
+                self.notchHeight = targetHeight
                 self.isVisible = true
             }
         }
@@ -169,8 +174,8 @@ public class DynamicNotch: ObservableObject {
         }
 
         let notchSize = DynamicNotch.getNotchSize(screen: screen)
-        self.notchWidth = customWidth ?? notchSize.width
-        self.notchHeight = customHeight ?? notchSize.height
+        self.notchWidth = notchSize.width
+        self.notchHeight = notchSize.height
     }
 
     private func initializeWindow(screen: NSScreen) {
